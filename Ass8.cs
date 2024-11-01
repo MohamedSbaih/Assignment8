d65577423547b2f9d6163d80c12da873d45fd7ee
Mohamed Ayman Sbaih <42489262+MohamedSbaih@users.noreply.github.com>
2024-11-01 19:49:26 +0200

    Add files via upload

---
+++ Ass8.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ass7 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    while(true){
+
+        int number = Random.Range(1,21);
+        if(number == 5)continue;
+        else if(number == 15) break;
+        else{
+            Debug.Log(number);
+            
+            {
+                
+            }
+        }
+        
+             }}
+
+    
+}
